e87ab2918daa086aff8c74e4729ed2b62a987223
armagedon444 <tomasz@mosur.net>
2017-11-11 18:00:15 +0100

    js generate things site

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,83 @@
 var itemType = 0;
 var itemNo = 0;
-const ammountOfItemType = 3;
-const ammountOfItems = [5,4,3];
+const ammountOfItems = [5, 2, 7];
+const itemsType = ["Image", "Video", "Gif"];
+
+
+function writeToTest()
+{
+    document.writeln("<h1> Things Generator</h1>"); 
+}
+
+function initializeListeners()
+{
+    document.getElementById("randIt").addEventListener("click", randomButtonListener);
+    document.getElementById("nextIt").addEventListener("click", nextButtonListener);
+    document.getElementById("itemNoB").addEventListener("click", selectItem);
+}
 
 function start()
 {
-    window.writeln("<h1>Aplikacja losująca fajną rzecz<h1>");   
+    initializeListeners();
+    randItem(true);
+    showRandType();
+    showItem();
+}
+
+function showRandType()
+{
+    document.getElementById("item-type").innerHTML="<h2>Current Item Type: " + itemsType[itemType] + "</h2>";
 }
 
 function randType()
 {
-    Math.round(Math.random()*ammountOfItemType);
+    itemType = Math.floor(Math.random()*ammountOfItems.length);
 }
 
-function randItem(Boolean randType = false)
+function randItem(randT = false)
 {
-    if (randType) randType();
-    itemNo = Math.round(Math.random()* ammountOfItems[itemType]);
+    if (randT) randType();
+    itemNo = Math.floor(Math.random()* ammountOfItems[itemType]);
+}
+
+function showItem()
+{
+    document.getElementById("item-No").innerHTML="<h2>Item Number:" + itemNo + "</h2>";
+        
+    switch(itemsType[itemType])
+        {
+        case "Image":
+                document.getElementById("content").innerHTML = "<img src=media/image/" +itemNo +".jpg />"; 
+                break;
+        case "Video":
+                document.getElementById("content").innerHTML = "<video controls> <source src=media/video/" + itemNo + ".webm type=video/webm> </video>"
+                break;       
+        case "Gif":
+                document.getElementById("content").innerHTML = "<img src=media/gif/" +itemNo +".gif />"; 
+                break; 
+        }
+
+}
+
+function randomButtonListener()
+{
+    randItem(true);
+    showRandType();
+    showItem();
+}
+
+function nextButtonListener()
+{
+    randItem();
+    showItem();
+}
+
+function selectItem()
+{
+    var itemNumber = window.prompt("Please select item: \n range: 0-" + ammountOfItems[itemType]);
+    if(itemNumber<0 || itemNumber > ammountOfItems[itemsType]) window.alert("Out of Bounds!");
+    else{
+        itemNo = itemNumber;
+    }
+    showItem();
 }
