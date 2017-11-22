113b8df27e5c4f63887ead70840ed38a088f4839
armagedon444 <tomasz@mosur.net>
2017-11-22 21:20:25 +0100

    init exercise

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,17 +1,24 @@
+function useFunctionExample()
+{
+    var p = document.createElement("p");
+    var WelcomeText = document.createTextNode("Hello in the JavaScript World");
+    p.appendChild(WelcomeText);
+    var button = document.createElement("button");
+    document.getElementById("header").appendChild(p);
+}
+
 function start()
 {
 	initializeListeners();
+    useFunctionExample();
 }
 
 function initializeListeners()
 {
-	document.getElementById("firstBOX").addEventListener("onmousedown", changeBoxBackground(event));
-	
+	//document.getElementById("firstBOX").addEventListener("onmousedown", changeBoxBackground(event));
 }
 
 
 function changeBoxBackground(event){
-	alert = "fuck"
-}
-	
+	alert = "fuck";
 }
