6dd68ddabfdd4ea74b196fc998c4262b1b4125e5
armagedon444 <tomasz@mosur.net>
2017-11-22 22:11:05 +0100

    MADE FIRST excercise

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,3 +1,6 @@
+//zadanie1 DONE
+
+
 function useFunctionExample()
 {
     var p = document.createElement("p");
@@ -5,6 +8,30 @@ function useFunctionExample()
     p.appendChild(WelcomeText);
     var button = document.createElement("button");
     document.getElementById("header").appendChild(p);
+    
+    
+    var footer =document.getElementById("footer");
+    
+//textToAdd
+    var footerText = document.createTextNode("Copyright by Pawel Gluszczak & Tomek 2017");
+    var footerP = document.createElement("p");
+    p.appendChild(footerP);
+    
+//insert Before
+    footer.insertBefore(footerText, footer.childNodes[0]);
+//remove child
+    footer.removeChild(footer.childNodes[1]);
+    
+//replacing Child
+    var textContainer = document.getElementById("text").parentElement;     //var textContainer = document.getElementById("textContainer");
+    
+     // create Text to replace
+     var footerTextReplace = document.createTextNode("Lorem ipsum keszke w mesze");
+     var textP = document.createElement("p");
+     textP.appendChild(footerTextReplace);
+    
+    textContainer.replaceChild(textP, textContainer.childNodes[0]);
+    
 }
 
 function start()
